ade07393f0ed148924789a2ef84bf6f43351ef51
jaseth <125108910+jaseth@users.noreply.github.com>
2023-11-07 10:34:01 -0500

    Delete parcial directory

--- parcial/app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-var app = angular.module('myApp', []);
-
-app.controller('BannerController', function($scope) {
-    $scope.bannerTitle = 'Física';
-    $scope.bannerText = 'Explorando el mundo a través de las leyes de la naturaleza';
-});
